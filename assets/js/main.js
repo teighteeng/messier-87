29ac494c5df0007f7e67be947e137194b3e66b31
Thanapong Yingyong <thanapongtle@gmail.com>
2019-04-29 12:26:03 +0700

    👨🏻‍💻 update: ~75%

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,10 @@
-const app = new Vue({
-    el: '#main',
-    data: {
-        message: 'Hello Vue!'
-    }
-})
+const dataJson =
+    '{ "screen01" : [' +
+        '{ "description" : "is a supergiant elliptical galaxy in the constellation Virgo. One of the most massive galaxies in the local Universe, it has a large population of globular clusters—about 12,000 compared with the 150–200 orbiting the Milky Way—and a jet of energetic plasma that originates at the core and extends at least 1,500 parsecs (4,900 light-years), traveling at relativistic speed." } ]}'
 
-console.log(app);
+datas = JSON.parse(dataJson)
+
+sc1Descrip = document.getElementById("screen_01_deception")
+sc1Descrip.innerHTML = datas.screen01[0].description
+
+console.log(datas.screen01[0].description)
